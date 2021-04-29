1a3f74dd0ff09b6ebafefedc4c04b636ea8676e6
kirychukyurii <yurii.kirychuk.knz.2017@lpnu.ua>
2021-04-29 15:53:50 +0300

    translated duration block

--- functions/func.js
+++ functions/func.js
@@ -74,11 +74,11 @@ sendAlert = async (alert, userId) => {
   if (alert.status == "resolved") {
     start = Date.parse(alert.startsAt);
     end = Date.parse(alert.endsAt);
-    text = `✅ ${alert.Annotations.message_resolved}\n<a href="${alert.GeneratorURL}">${alert.Annotations.identifier}</a>\n${parseTime(alert.startsAt)}\nDuration: ${getDuration(start, end)}`;
+    text = `✅ ${alert.Annotations.message_resolved}\n<a href="${alert.GeneratorURL}">${alert.Annotations.identifier}</a>\n${parseTime(alert.startsAt)}\nТривалість: ${getDuration(start, end)}`;
   } else {
     start = Date.parse(alert.startsAt);
     end = Date.now();
-    text = `🔥 ${alert.Annotations.message_firing}\n<a href="${alert.GeneratorURL}">${alert.Annotations.identifier}</a>\n${parseTime(alert.startsAt)}\n${getDuration(start, end)} ago`;
+    text = `🔥 ${alert.Annotations.message_firing}\n<a href="${alert.GeneratorURL}">${alert.Annotations.identifier}</a>\n${parseTime(alert.startsAt)}\n${getDuration(start, end)} тому`;
   }
 
   returnData = [alert.labels.alertname, alert.labels.instance];
